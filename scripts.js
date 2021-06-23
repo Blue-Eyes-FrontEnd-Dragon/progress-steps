67af55a9baf53e4de0e54018a0995f619bb6c397
LukeMcCann <developer-lukemccann@outlook.com>
2021-06-23 20:31:21 +0100

    Add next function

---
+++ scripts.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+    currentActive > circles.length-1 ? currentActive = circles.length : currentActive++;
+    
+    update();
+});
+
+prev.addEventListener('click', () => {
+    currentActive < 1 ? currentActive = 1 : currentActive--;
+
+    update();
+});
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    const actives = document.querySelectorAll('.active');
+
+    console.log(calculatePercentage(actives.length, circles.length));
+
+    progress.style.width = calculatePercentage(actives.length-1, circles.length-1) + '%';
+
+}
+
+function calculatePercentage(a, b) {
+    return (a / b) * 100;
+}
